d2324c61da30857d5107d43e35d198f583617052
Helix <267227783+helix-nine@users.noreply.github.com>
2026-07-24 03:36:31 +0000

    chore: bump start-sdk to 2.0.7; refresh mempool-startos,tor-startos pin(s); 0.35.8:0 → 0.35.8:1

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -3,12 +3,12 @@ import { sdk } from './sdk'
 export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
   mempool: {
     kind: 'running',
-    versionRange: '>=3.3.1:15',
+    versionRange: '>=3.3.1:18',
     healthChecks: ['webui'],
   },
   tor: {
     kind: 'running',
-    versionRange: '>=0.4.9.11:2',
+    versionRange: '>=0.4.9.11:4',
     healthChecks: ['tor'],
   },
 }))
